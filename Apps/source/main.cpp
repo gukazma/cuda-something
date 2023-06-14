445fb5519db15f365eae0fee27257aabec639841
geekgary-x <18017158729@163.com>
2023-06-14 10:30:40 +0800

    ✨ feat: only cuda

--- Apps/source/main.cpp
+++ Apps/source/main.cpp
@@ -31,24 +31,10 @@
 #include "cuda_runtime.h"
 #include "cuda.h"
 #include <iostream>
-#define NO_NVML
-#ifndef NO_NVML
-#include "nvml.h"
-#endif // ndef NO_NVML
-
 #define CUDA_CALL(function, ...)  { \
     cudaError_t status = function(__VA_ARGS__); \
     anyCheck(status == cudaSuccess, cudaGetErrorString(status), #function, __FILE__, __LINE__); \
 }
-
-#ifndef NO_NVML
-#define NVML_CALL(function, ...)  { \
-    nvmlReturn_t status = function(__VA_ARGS__); \
-    anyCheck(status == NVML_SUCCESS, nvmlErrorString(status), #function, __FILE__, __LINE__); \
-}
-#else // ndef NO_NVML
-#define NVML_CALL(function, ...) { } // We create dummy wrapper to skip initialization code etc.
-#endif // ndef NO_NVML
 
 void anyCheck(bool is_ok, const char *description, const char *function, const char *file, int line) {
     if (!is_ok) {
@@ -66,56 +52,19 @@ void getMemoryUsageCUDA(int deviceId, size_t &memUsed, size_t &memTotal) {
     memTotal = memTotal / 1024 / 1024;
 }
 
-#ifndef NO_NVML
-void getMemoryUsageNVML(nvmlDevice_t &nvmlDevice, size_t &memUsed, size_t &memTotal) {
-    nvmlMemory_t nvmlMemory;
-    NVML_CALL(nvmlDeviceGetMemoryInfo, nvmlDevice, &nvmlMemory);
-    memUsed = nvmlMemory.used / 1024 / 1024;
-    memTotal = nvmlMemory.total / 1024 / 1024;
-}
-#endif
-
 int main() {
     int cudaDeviceCount;
     struct cudaDeviceProp deviceProp;
     size_t memUsed, memTotal;
-#ifndef NO_NVML
-    unsigned int nvmlDeviceCount;
-    nvmlPciInfo_t nvmlPciInfo;
-    nvmlDevice_t nvmlDevice;
-#endif // ndef NO_NVML
-
     CUDA_CALL(cudaGetDeviceCount, &cudaDeviceCount);
-    NVML_CALL(nvmlInit);
-    NVML_CALL(nvmlDeviceGetCount, &nvmlDeviceCount);
 
     for (int deviceId = 0; deviceId < cudaDeviceCount; ++deviceId) {
         CUDA_CALL(cudaGetDeviceProperties, &deviceProp, deviceId);
         printf("Device %2d", deviceId);
-#ifndef NO_NVML
-        int nvmlDeviceId = -1;
-        for (int nvmlId = 0; nvmlId < nvmlDeviceCount; ++nvmlId) {
-            NVML_CALL(nvmlDeviceGetHandleByIndex, nvmlId, &nvmlDevice);
-            NVML_CALL(nvmlDeviceGetPciInfo, nvmlDevice, &nvmlPciInfo);
-            if (deviceProp.pciDomainID == nvmlPciInfo.domain &&
-                deviceProp.pciBusID    == nvmlPciInfo.bus    &&
-                deviceProp.pciDeviceID == nvmlPciInfo.device) {
-
-                nvmlDeviceId = nvmlId;
-                break;
-            }
-        }
-        printf(" [nvidia-smi %2d]", nvmlDeviceId);
-#endif // ndef NO_NVML
-
         printf(" [PCIe %04x:%02x:%02x.0]", deviceProp.pciDomainID, deviceProp.pciBusID, deviceProp.pciDeviceID);
         printf(": %20s (CC %d.%d)", deviceProp.name, deviceProp.major, deviceProp.minor);
 
-#ifndef NO_NVML
-        getMemoryUsageNVML(nvmlDevice, memUsed, memTotal);
-#else //  ndef NO_NVML
         getMemoryUsageCUDA(deviceId, memUsed, memTotal);
-#endif // ndef NO_NVML
         printf(": %5zu of %5zu MiB Used", memUsed, memTotal);
 
         // »ñÈ¡ GPU ÎÂ¶È
@@ -140,8 +89,6 @@ int main() {
         std::cout << "GPU Temperature: " << temperature << " degrees Celsius" << std::endl;
         printf("\n");
     }
-
-    NVML_CALL(nvmlShutdown);
     return 0;
 }
 
